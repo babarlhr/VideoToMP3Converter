b0dd3703367d48acf364a80db3425927e4cdb719
ishfaq babar <ishfaq_ciit@yahoo.com>
2022-10-27 09:46:29 +0500

    Add a button

--- Mp3Converter.Designer.cs
+++ Mp3Converter.Designer.cs
@@ -28,13 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnOpenFile = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(487, 98);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(110, 37);
+            this.btnOpenFile.TabIndex = 0;
+            this.btnOpenFile.Text = "Open File";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.button1_Click);
             // 
             // Mp3Converter
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnOpenFile);
             this.Name = "Mp3Converter";
             this.Text = "Video To Mp3 Converter";
             this.Load += new System.EventHandler(this.MP3ConverterForm_Load);
@@ -43,6 +55,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnOpenFile;
     }
 }
 
--- Mp3Converter.cs
+++ Mp3Converter.cs
@@ -21,5 +21,10 @@ namespace VideoToMP3Converter
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
